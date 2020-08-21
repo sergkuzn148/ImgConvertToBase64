97e9da659a6e7af8940a7d3915a38023a6ff1aa9
sergkuzn148 <31191392+sergkuzn148@users.noreply.github.com>
2020-08-21 12:52:25 +0300

    Updates

--- Program.cs
+++ Program.cs
@@ -1,21 +1,20 @@
 ﻿using System;
-using System.Drawing;
-using System.Collections.Generic;
-using System.Data;
-using System.IO;
-using System.Threading.Tasks;
 
-
-namespace ImgToBase64
+namespace ImgConvertToBase64
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            byte[] imageArray = System.IO.File.ReadAllBytes(@"/Users/sergeykuznetsov/Documents/GitHub/ImgToBase64/testPicture.jpg");
+            byte[] imageArray = System.IO.File.ReadAllBytes(@"/Users/sergeykuznetsov/Documents/GitHub/ImgToBase64/ImgConvertToBase64/testPicture.jpg");
+            
             string base64String = Convert.ToBase64String(imageArray);
             Console.WriteLine(base64String);
+            
+            //Console.WriteLine("Encode");
+            //var base64EncodedBytes = System.Convert.FromBase64String(base64String);
+            //string encode = System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+            //Console.WriteLine(encode);
         }
     }
 }
