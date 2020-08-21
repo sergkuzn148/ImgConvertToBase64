7a9cda1ff02ab283040694e6e95f8d43598922c6
sergkuzn148 <31191392+sergkuzn148@users.noreply.github.com>
2020-08-21 10:27:13 +0300

    upload and convert

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace ImgToBase64
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            byte[] imageArray = System.IO.File.ReadAllBytes(@"/Users/sergeykuznetsov/Documents/GitHub/ImgToBase64/testPicture.jpg");
+            string base64String = Convert.ToBase64String(imageArray);
+            Console.WriteLine(base64String);
+        }
+    }
+}
